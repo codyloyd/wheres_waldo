e80c3dcd9db829fecf9a16adde500b204fc128d6
Cody Loyd <codyloyd@gmail.com>
2016-09-20 19:54:02 -0500

    works

--- app/assets/javascripts/puzzles.js
+++ app/assets/javascripts/puzzles.js
@@ -10,7 +10,11 @@ $(document).ready(function(){
 		var y = e.pageY - $(this).offset().top
 		$(this).append("<div class='target'></div>")
 		target = document.querySelector("div.target")
-		$(target).css({"top": y,"left": x})
-		$(target).animate({height: "50px", width: "50px", top: "-=30", left: "-=30"},200)
+		$(target).css({"top": y-5,"left": x-5})
+		$(target).animate({height: "50px", width: "50px", top: "-=25", left: "-=25"},200, function(){
+			if (x > 533 && x < 562 && y > 145 && y < 183) {
+				alert("WIN")
+			}
+		})
 	})
 })
