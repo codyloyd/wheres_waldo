a700314dae68a1ad8d88ea5b91f46afe724333a0
Cody Loyd <codyloyd@gmail.com>
2016-09-27 14:06:27 -0500

    js

--- app/assets/javascripts/puzzles.js
+++ app/assets/javascripts/puzzles.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var time = new Date().getTime()
 	var solution_x = gon.solution_x
 	var solution_y = gon.solution_y
 	var img = document.getElementById("puzzle-image")
@@ -16,7 +17,14 @@ $(document).ready(function(){
 		$(target).css({"top": y-5,"left": x-5})
 		$(target).animate({height: "50px", width: "50px", top: "-=25", left: "-=25"},200, function(){
 			if (x > solution_x - 20 && x < solution_x + 20 && y > solution_y - 20 && y < solution_y + 20) {
-				alert("WIN")
+				var winTime = new Date().getTime() - time
+				// alert("WIN in " + (winTime/1000).toFixed(1) + " seconds")
+				var div = document.createElement("div")
+				$(div).addClass("win-prompt")
+				$("body").append(div)
+				$(".win-prompt").append("<h1>you found him!</h1> <h2> and it only took you " + (winTime/1000).toFixed(1) + " seconds!</h2>")
+				$(".win-prompt").append("<button>Next Puzzle?</button>")
+
 			}
 		})
 	})
