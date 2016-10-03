085933f060244704665739b3a76721d3b3899a78
Cody Loyd <codyloyd@gmail.com>
2016-10-02 21:06:33 -0500

    added score view

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,4 +4,10 @@ class ScoresController < ApplicationController
     @puzzle.scores.create(score: params[:score][:time], name: params[:score][:name])
     redirect_to puzzles_url
   end
+
+  def puzzle_scores
+    @puzzle = Puzzle.find(params[:id])
+    @scores = @puzzle.scores.all.order(score: :asc)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'puzzles#index'
   get 'puzzles/:id/win_prompt/:time', :to => 'puzzles#win_prompt'
+  get 'puzzles/:id/scores', :to => 'scores#puzzle_scores', as: "high_scores"
   resources :puzzles
   resources :scores
 end
